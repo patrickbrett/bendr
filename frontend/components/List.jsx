0f04180c9446e8ba49b72eadcc098b0bc8b461f5
Patrick Brett <pkbrett37@gmail.com>
2019-03-16 17:46:39 +1100

    add places query

--- frontend/components/List.jsx
+++ frontend/components/List.jsx
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 
 class List extends Component {
   render() {
-    return (<div />);
+    return (<div id="barList">
+      <div className="formComponent"><input type="text" id="location" placeholder="Enter location..." /></div>
+      <div className="formComponent"><input type="text" id="instrument" placeholder="Enter instrument..." /></div>
+    </div>);
   }
 }
 
--- frontend/components/PageContent.jsx
+++ frontend/components/PageContent.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 import Map from "./Map.jsx";
-import Form from "./Form.jsx";
+import Form from "./List.jsx";
 import List from "./List.jsx";
 import data from "../assets/data.json";
+import {apiKey} from "../apiKey.json";
 
 class PageContent extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      bands: data.bands
+      chosenBars: [
+
+      ]
+    };
+
+    this.retrieveBars = () => {
+      const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${apiKey}&radius=1000&location=-37.812609,144.958966&keyword=bar`;
     };
   }
 
