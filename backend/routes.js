0c1c2f203cabbdc35d3724fb300948cf726227c0
Patrick Brett <pkbrett37@gmail.com>
2019-03-16 13:24:59 +1100

    Minor bugfixes for first webpack build

--- backend/routes.js
+++ backend/routes.js
@@ -1,12 +1,5 @@
-const Loader = require("./Loader");
-const ApiHandler = require("./ApiHandler");
-
 const routes = (app) => {
-    app.get("/service/session/:sessionUrl", Loader.loadSession);
-    app.get("/service/checkin/:sessionId", Loader.loadCheckin);
 
-    app.post("/api/session", ApiHandler.createSession);
-    app.post("/api/checkin", ApiHandler.createCheckin);
 }
 
 module.exports = routes;
