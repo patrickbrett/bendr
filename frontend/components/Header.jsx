39289d3a4c4b5b9858b53e23e640bbb49d4dd7b8
Andy <azha0018@student.monash.edu>
2019-03-16 15:41:31 +1100

    add logo

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Header extends Component {
   render() {
     return (<div id="header">
-      <h1>Tutti</h1>
+      <h1><img src="assets/tutti_logo.png" /></h1>
     </div>);
   }
 }
