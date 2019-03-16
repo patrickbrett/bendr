00f5d316f13b581e1ed5160c15a185fc9dc9a29f
Andy <azha0018@student.monash.edu>
2019-03-17 00:26:08 +1100

    search bar

--- frontend/components/Search.jsx
+++ frontend/components/Search.jsx
@@ -4,8 +4,8 @@ class Search extends Component {
   render() {
     return (
       <div id="searchContainer">
-        <input type="text" onChange={this.props.updateSearch} />
-        <button onClick={this.props.performSearch}>Go</button>
+        <input className = "searchBar" type="text" onChange={this.props.updateSearch} />
+        <button className = "searchButton" onClick={this.props.performSearch}>Go</button>
       </div>
     );
   }
