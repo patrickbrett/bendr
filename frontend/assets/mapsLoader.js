775aa56b29baa8882e06dce09328312b51e2d2e3
Patrick Brett <pkbrett37@gmail.com>
2019-03-16 15:39:16 +1100

    use mapsLoader only in public folder

--- frontend/assets/mapsLoader.js
+++
@@ -1,4 +0,0 @@
-var script = document.createElement('script'),
-  scripts = document.getElementsByTagName('script')[0];
-script.src = 'https://maps.googleapis.com/maps/api/js?key=API_KEY_HERE';
-scripts.parentNode.insertBefore(script, scripts);
