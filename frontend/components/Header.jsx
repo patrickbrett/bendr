330d46cab9ee01b93a9a8ff0c29fbfad01e23cf1
Andy <azha0018@student.monash.edu>
2019-03-16 15:52:58 +1100

    modify header image

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Header extends Component {
   render() {
     return (<div id="header">
-      <h1><img src="assets/tutti_logo.png" /></h1>
+      <h1><img id="logo" src="assets/tutti_logo.png" /></h1>
     </div>);
   }
 }
